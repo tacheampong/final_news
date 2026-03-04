5976ceafebb4322e13489b2e807e8f2f2af491a4
tacheampong <th.acheampong@gmail.com>
2026-03-04 11:53:59 -0500

    modify margins back to 20 80 70 20

--- final-new-app/src/YoutubeLikesLineChart.js
+++ final-new-app/src/YoutubeLikesLineChart.js
@@ -2,7 +2,7 @@ import * as d3 from 'd3'
 import LineChart from './LineChart'
 import Legend from './LineChartLegend';
 const YoutubeLikesLineChart = ({data}) => {
-  var margin = { top: 20, left: 60, bottom: 20, right: 20 };
+  var margin = { top: 20, left: 80, bottom: 70, right: 20 };
    var width = 500 - margin.left - margin.right;
    var height = 500 - margin.top - margin.bottom;
  
